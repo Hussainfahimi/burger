9cd3c10dd8e5243a0886c09408a3f8354f9bcfdb
Hussain Fahimi <hussain.fahimi@gmail.com>
2018-10-27 14:12:57 -0700

    jaws init

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,24 @@
 // Set up MySQL connection.
-var mysql = require("mysql");
+var mysql = require('mysql');
+var connection;
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else {
+  connection.mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'burgers_db'
+  });
+};
 
 // Make connection.
-connection.connect(function(err) {
+connection = connect(function(err) {
   if (err) {
-    throw err;
+     throw err;
     
-  }
+  };
   console.log("connected as id: " + connection.threadId);
 });
 
